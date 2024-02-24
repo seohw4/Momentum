d6de3c55faf5fd3df3527258359b64d14483a048
편민경 <pyeonmingyeong@pyeonmingyeong-ui-MacBookAir.local>
2024-02-24 15:32:12 +0900

    form submission

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function loginButtonClick() {
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", loginButtonClick);
