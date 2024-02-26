24c474fd7a2704fef16941f20ae5c988c24abd16
편민경 <pyeonmingyeong@pyeonmingyeong-ui-MacBookAir.local>
2024-02-26 15:24:31 +0900

    events part 2

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function loginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", loginSubmit);
+link.addEventListener("click", handleLinkClick);
