5f2029075b32a0086985f3de8184d3c12a40b463
seohw4 <jerry8342@gmail.com>
2024-02-27 23:35:53 +0900

    timeouts and dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function blinkCat() {
-  console.log("meow");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(blinkCat, 2000);
+getClock();
+setInterval(getClock, 1000);
