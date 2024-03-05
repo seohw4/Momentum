4871dad16bb938a87c6993fd405af4248a72859c
seohw4 <jerry8342@gmail.com>
2024-03-05 17:28:27 +0900

    deleting todos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+
   toDoList.appendChild(li);
 }
 
